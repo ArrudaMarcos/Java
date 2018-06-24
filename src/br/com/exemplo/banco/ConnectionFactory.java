4cd652741f582931fcd626a221d93ff322d71e58
marco_wr@outlook.com <marco_wr@outlook.com>
2018-06-23 21:28:04 -0300

    Correção banco de dados

--- src/br/com/exemplo/banco/ConnectionFactory.java
+++ src/br/com/exemplo/banco/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
 	public Connection getConnection(){
 	   try {
 		   Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		   return DriverManager.getConnection("jdbc:sqlserver://meusbancos.database.windows.net:1433;database=MoniWeb;user=redicopa@meusbancos;password={Acesso16};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+		   return DriverManager.getConnection("jdbc:sqlserver://meusbancos.database");
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
